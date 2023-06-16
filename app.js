dd5af820647e0fe42b5b1140c2fc89bc5fa9ae51
iamjesusc <131019431+iamjesusc@users.noreply.github.com>
2023-06-16 15:47:22 -0500

    Subiendo "version_2.1"

--- app.js
+++ app.js
@@ -1,110 +1,97 @@
 function encrypt() {
-    
-    let contentCipher = document.querySelector(".content__cipher");
-    let phrase = contentCipher.value;
+  let contentCipher = document.querySelector(".content__cipher");
+  let phrase = contentCipher.value;
 
-    if(phrase !== "") {
-    
-        let encripted = phrase
-        .replace(/e/g, "enter")
-        .replace(/i/g, "imes")
-        .replace(/a/g, "ai")
-        .replace(/o/g, "ober")
-        .replace(/u/g, "ufat");
+  if (phrase !== "") {
+    let encripted = phrase
+      .replace(/e/g, "enter")
+      .replace(/i/g, "imes")
+      .replace(/a/g, "ai")
+      .replace(/o/g, "ober")
+      .replace(/u/g, "ufat");
 
-        let contetView = document.querySelector(".content__view");
-        contetView.value = encripted;
-        contetView.style.background = "none";
+    let contetView = document.querySelector(".content__view");
+    contetView.value = encripted;
+    contetView.style.background = "none";
 
-        let descriptionView = document.querySelector(".description__content");
-        descriptionView.style.display = "none";
+    let descriptionView = document.querySelector(".description__content");
+    descriptionView.style.display = "none";
 
-        let  btnCopy = document.querySelector(".btn__copy");
-        btnCopy.style.display = "flex";
+    let btnCopy = document.querySelector(".btn__copy");
+    btnCopy.style.display = "flex";
 
-        let btnDecrypt = document.querySelector(".btn__decrypt");
-        btnDecrypt.style.background = "white";
-        
-        return encripted;
-   
-    } else {
+    let btnDecrypt = document.querySelector(".btn__decrypt");
+    btnDecrypt.style.background = "white";
 
-        alert("Ingresa el texto que desees encriptar o desencriptar.");
-
-    }
-    
+    return encripted;
+  } else {
+    alert("Ingresa el texto que desees encriptar o desencriptar.");
+  }
 }
 
 function decrypt() {
+  let contentCipher = document.querySelector(".content__cipher");
+  let encripted = contentCipher.value;
 
-    let contentCipher = document.querySelector(".content__cipher");
-    let encripted = contentCipher.value;
-    
-    if(encripted !== "") {
+  if (encripted !== "") {
+    let decrypted = encripted
+      .replace(/enter/g, "e")
+      .replace(/imes/g, "i")
+      .replace(/ai/g, "a")
+      .replace(/ober/g, "o")
+      .replace(/ufat/g, "u");
 
-        let decrypted = encripted
-        .replace(/enter/g, "e")
-        .replace(/imes/g, "i")
-        .replace(/ai/g, "a")
-        .replace(/ober/g, "o")
-        .replace(/ufat/g, "u");
+    let contetView = document.querySelector(".content__view");
+    contetView.value = decrypted;
+    contetView.style.background = "none";
 
-        let contetView = document.querySelector(".content__view");
-        contetView.value = decrypted;
-        contetView.style.background = "none";
+    let descriptionView = document.querySelector(".description__content");
+    descriptionView.style.display = "none";
 
-        let descriptionView = document.querySelector(".description__content");
-        descriptionView.style.display = "none";
+    let btnDecrypt = document.querySelector(".btn__decrypt");
+    btnDecrypt.style.background = "white";
 
-        let btnDecrypt = document.querySelector(".btn__decrypt");
-        btnDecrypt.style.background = "white";
+    let btnCopy = document.querySelector(".btn__copy");
+    btnCopy.style.display = "flex";
 
-        let  btnCopy = document.querySelector(".btn__copy");
-        btnCopy.style.display = "flex"
-        
-        return decrypted;
-
-    } else {
-        alert("Ingresa el texto que desees encriptar o desencriptar.");
-    }
-    
+    return decrypted;
+  } else {
+    alert("Ingresa el texto que desees encriptar o desencriptar.");
+  }
 }
 
 function copyEncryption() {
-    let contentView = document.querySelector(".content__view");
-    contentView.select();
-    navigator.clipboard.writeText(contentView.value);
-    alert("Mensaje copiado");
-    
-    let contentCipher = document.querySelector(".content__cipher");
-    contentCipher.value = contentView.value;
+  let contentView = document.querySelector(".content__view");
+  contentView.select();
+  navigator.clipboard.writeText(contentView.value);
+  alert("Mensaje copiado");
 
-    contentView.value = "";
-    contentView.style.background = "";
+  let contentCipher = document.querySelector(".content__cipher");
+  contentCipher.value = contentView.value;
 
-    
-    let descriptionView = document.querySelector(".description__content");
-    descriptionView.style.display = "";
-    
+  contentView.value = "";
+  contentView.style.background = "";
 
-    let  btnCopy = document.querySelector(".btn__copy");
-    btnCopy.style.display = "";
+  let descriptionView = document.querySelector(".description__content");
+  descriptionView.style.display = "";
 
-    let btnDecrypt = document.querySelector(".btn__decrypt");
-        btnDecrypt.style.background = "";
+  let btnCopy = document.querySelector(".btn__copy");
+  btnCopy.style.display = "";
 
+  let btnDecrypt = document.querySelector(".btn__decrypt");
+  btnDecrypt.style.background = "";
 }
 
 function validateText() {
-    let contentCipher = document.querySelector(".content__cipher");
-    let text = contentCipher.value;
-     
-    if (text !== "") {    
-      const validate = /^[a-zñ\s]+$/;
-  
-      if (!validate.test(text)) {
-        contentCipher.value = text.replace(/[^a-zñ\s]/g, ''); 
-        alert("Solo letras minúsculas y sin acentos")
-      }
+  let contentCipher = document.querySelector(".content__cipher");
+  let text = contentCipher.value;
+
+  if (text !== "") {
+    const validate = /^[a-zñ\s]+$/;
+
+    if (!validate.test(text)) {
+      contentCipher.value = text.replace(/[^a-zñ\s]/g, "");
+      alert("Solo letras minúsculas y sin acentos");
     }
-  } 
+  }
+}
